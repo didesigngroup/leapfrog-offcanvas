1ebb3397b1cc80b3250d0ac43536523c41c451cf
didesigngroup <quinn.reddy@digitalinsight.com>
2017-05-10 23:16:52 -0400

    CUI-39 Datepicker + Tooltips updates

--- upgrade/js/tooltips.js
+++ upgrade/js/tooltips.js
@@ -1,5 +1,11 @@
  
  // Tooltips Script
 $(document).ready(function(){
-    $('[data-toggle="popover"]').popover(); 
+    $('[data-toggle="popover"]').popover({
+        html : true,
+        title : '<a href="#" class="close" data-dismiss="alert"><span class="icon icon-sm-close icon-sm-close-dims"></span></a>'
+    });
+    $(document).on("click", ".popover .close" , function(){
+        $(this).parents(".popover").popover('hide');
+    });
 });
